55d94e32202a2347c8358bf9e80ca7fa425c3f09
Lawson Hung <lhung1@babson.edu>
2019-04-20 00:03:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,5 @@
 var playlist = new Object({ Adele: 'Hello' }, { JasonDerulo: 'Cheyenne' }, { Skillet: 'Monster'});
+
+function updatePlayList(playList, artistName, songTitle) {
+  return playList['artistName'] = 'songTitle';
+}
